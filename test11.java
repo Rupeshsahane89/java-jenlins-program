e8625106a12eec68aa2e71460be98f7764f7cc20
Rupeshsahane89 <154776703+Rupeshsahane89@users.noreply.github.com>
2024-01-16 11:43:54 +0530

    added one more file

--- test11.java
+++ test11.java
@@ -5,7 +5,7 @@ public class test11
     public static void main(String[] args) {
         
         System.out.println("This is a java program in jenkins folder");
-        System.out.println("Hello wolrd");
+        System.out.println("Hello world");
         System.out.println(new Date());
         System.out.println("Welcome to ASMA collage");
     }
