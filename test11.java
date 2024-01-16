4bff57e313928f9c882d1c157bd318b9d06c5a18
Rupeshsahane89 <154776703+Rupeshsahane89@users.noreply.github.com>
2024-01-16 11:18:42 +0530

    Initial commit

---
+++ test11.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class test11
+{
+    public static void main(String[] args) {
+        
+        System.out.println("This is a java program in jenkins folder");
+        System.out.println("Hello wolrd");
+        System.out.println(new Date());
+    }
+}
